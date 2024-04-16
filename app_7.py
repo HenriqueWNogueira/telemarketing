c4d6507ead01785c34a455ee1cf5bd07b4ce35cd
HenriqueWNogueira <143613189+HenriqueWNogueira@users.noreply.github.com>
2024-04-16 15:15:15 -0300

    Update app_7.py

--- app_7.py
+++ app_7.py
@@ -54,9 +54,7 @@ def main():
 
     # Título principal da aplicação
     st.write('# Análise dos dados de Telemarketing')
-    st.write('# Análise dos dados de Telemarketing')
-    st.write('# Análise dos dados de Telemarketing')
-	
+
     st.markdown("---")
     
     # Apresenta a imagem na barra lateral da aplicação
